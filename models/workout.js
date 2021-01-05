a0683f9b6ccbd24f38911c64887258178c1a4ae6
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2021-01-04 22:31:23 -0600

    models folder

---
+++ models/workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+    },
+
+    exercises:[
+        {type: {
+            type: String,
+        },
+        name: {
+            type: String
+        }
+        }
+
+    ]    
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { u
 
 //view
 app.get("/exercise", (req, res) => {
-     res.sendFile(path.join(__dirname, "/public/exercise.html"));
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
