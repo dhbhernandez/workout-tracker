8494ab9f22968eba1eed4a1c9c8a8a768907d158
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2021-01-04 21:07:18 -0600

    connect front end with back end

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require("./userModel.js");
+const app = express();
+const path = require('path')
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+
+//view
+app.get("/exercise", (req, res) => {
+     res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+
+
+// app.post("/submit", ({ body }, res) => {
+//     User.create(body)
+//         .then(dbUser => {
+//             res.json(dbUser);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
